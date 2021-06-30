49520245ba9798fefb846ace115a99e2c33fa546
kaustubh-8727 <67389768+kaustubh-8727@users.noreply.github.com>
2021-06-30 15:23:31 +0530

    Create maximumtoffees.cpp

---
+++ day3/maximumtoffees.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i,j,n,k;
+    cin>>n>>k;
+    vector<int> v(n);
+    vector<string> ar(n);
+    for(i=0;i<n;i++){
+        cin>>v[i];
+    }
+    int m=*max_element(v.begin(), v.end());
+    for(i=0;i<n;i++){
+        if(v[i]+k>=m){
+            ar.push_back("true");
+        }
+        else{
+            ar.push_back("false");
+        }
+    }
+    for(i=0;i<n;i++){
+        cout<<ar[i]<<" ";
+    }
+    return 0;
+}
