82aca533739dbfd82f60ebe51c7a42d700ae87bc
kaustubh-8727 <67389768+kaustubh-8727@users.noreply.github.com>
2021-07-10 21:37:28 +0530

    Update Q1.cpp

--- Day 11/Q1.cpp
+++ Day 11/Q1.cpp
@@ -1,1 +1,11 @@
-.
+bool isPalindrome(string s) {
+        int i=0,j=s.length()-1;
+        while(i<j)
+        {
+            if(!iswalnum(s[i])) i++;
+            else if(!iswalnum(s[j])) j--;
+            else if(tolower(s[i])!=tolower(s[j])) return false;
+            else{i++;j--;}
+        }
+        return true;
+    }
