5b9578362e48fe5b76f3e4b3948555a172066c89
kaustubh-8727 <67389768+kaustubh-8727@users.noreply.github.com>
2022-01-26 14:14:06 +0530

    Update remove duplicates.cpp

--- day 1/remove duplicates.cpp
+++ day 1/remove duplicates.cpp
@@ -1,27 +1,34 @@
-#include<bits/stdc++.h>
-using namespace std;
+import java.util.*;
 
-int main(){
-    int i,j,n,k;
-    cin>>n;
-    vector<int> v(n);
-    for(i=0;i<n;i++){
-        cin>>v[i];
+class Java{
+    public static void main(String args[]){
+        System.out.println("Java Program");
     }
-    if(n==0){
-        cout<<n;
-        return 0;
-    }
-    i=0;
-    for (j = 1; j < v.size(); j++) {
-        if (v[j] != v[i]) {
-            i++;
-            v[i] = v[j];
-        }
-    }
-    k=i+1;
-    for(i=0;i<k;i++){
-        cout<<v[i]<<" ";
-    }
-    return 0;
 }
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int i,j,n,k;
+//     cin>>n;
+//     vector<int> v(n);
+//     for(i=0;i<n;i++){
+//         cin>>v[i];
+//     }
+//     if(n==0){
+//         cout<<n;
+//         return 0;
+//     }
+//     i=0;
+//     for (j = 1; j < v.size(); j++) {
+//         if (v[j] != v[i]) {
+//             i++;
+//             v[i] = v[j];
+//         }
+//     }
+//     k=i+1;
+//     for(i=0;i<k;i++){
+//         cout<<v[i]<<" ";
+//     }
+//     return 0;
+// }
